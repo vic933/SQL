ebd9c8120b0cc4c79b056c28fd32e4d476e6103b
vic933 <vicsanchezmartin@gmail.com>
2025-03-07 20:01:33 +0100

    Creación ejercicio 2 enuncido

---
+++ Retoguiado2_Qavanzadas.sql
@@ -0,0 +1,19 @@
+/*2.1. Ordena las películas por duración de forma ascendente*/
+
+/*2.2. Muestra los 10 clientes con mayor valor de id*/
+
+/*2.3. Encuentra lo que costó el antepenúltimo alquiler ordenado por día.*/
+
+/*2.4. Qué películas se alquilan por encima del precio medio. Para este ejercicio tendrás que generar dos queries diferentes. Una primera para calcular la media y la segunda para obtener las películas que se alquilan por encima de ese valor.*/
+
+/*5. Encuentra la cantidad total de películas en cada clasificación de la tabla film y muestra la clasificación junto con el recuento.*/
+
+/*2.6. Encuentra el promedio de duración de las películas para cada clasificación de la tabla film y muestra la clasificación junto con el promedio de duración.*/
+
+/*2.7. Números de alquiler por día. Ordenados por cantidad de alquiler de forma descendente.*/
+
+/*2.8. Averigua el número de alquileres registrados por mes.*/
+
+/*2.9. Número de películas por categoría estrenadas en 2006*/
+
+/*2.10. Muestra el id de los actores que hayan participado en más de 40 películas.*/
