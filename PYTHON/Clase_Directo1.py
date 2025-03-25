9072cf92b855cbdf3f58eea57704e6cf84b96a98
vic933 <vicsanchezmartin@gmail.com>
2025-03-25 20:57:21 +0100

    Create Clase_Directo1.py

---
+++ PYTHON/Clase_Directo1.py
@@ -0,0 +1,79 @@
+lista_campañas = [
+    {
+        'nombre': 'Rebajas de Enero',
+        'presupuesto': 35000,
+        'inicio': '2023-01-02',
+        'fin': '2023-01-31',
+        'medios': ['Publicidad en línea', 'Redes Sociales', 'Email Marketing'],
+        'segmentos_objetivo': ['Clientes regulares', 'Potenciales compradores'],
+        'personas_alcanzadas': 50000,
+        'tasa_conversion': 0.02
+    },
+    {
+        'nombre': 'San Valentín',
+        'presupuesto': 25000,
+        'inicio': '2023-02-01',
+        'fin': '2023-02-14',
+        'medios': ['Redes Sociales', 'Email Marketing', 'Publicidad impresa'],
+        'segmentos_objetivo': ['Parejas', 'Jóvenes'],
+        'personas_alcanzadas': 30000,
+        'tasa_conversion': 0.05
+    },
+    {
+        'nombre': 'Día del Padre',
+        'presupuesto': 20000,
+        'inicio': '2023-06-01',
+        'fin': '2023-06-15',
+        'medios': ['Redes Sociales', 'Colaboraciones con influencers', 'Publicidad impresa'],
+        'segmentos_objetivo': ['Jóvenes', 'Familias'],
+        'personas_alcanzadas': 25000,
+        'tasa_conversion': 0.03
+    },
+    {
+        'nombre': 'Día de la Madre',
+        'presupuesto': 22000,
+        'inicio': '2023-04-20',
+        'fin': '2023-05-10',
+        'medios': ['Publicidad en línea', 'Redes Sociales', 'Email Marketing'],
+        'segmentos_objetivo': ['Jóvenes', 'Familias'],
+        'personas_alcanzadas': 28000,
+        'tasa_conversion': 0.04
+    },
+    {
+        'nombre': 'Día del Friki',
+        'presupuesto': 18000,
+        'inicio': '2023-05-20',
+        'fin': '2023-05-25',
+        'medios': ['Publicidad en tiendas físicas', 'Redes Sociales', 'Colaboraciones con influencers'],
+        'segmentos_objetivo': ['Jóvenes'],
+        'personas_alcanzadas': 20000,
+        'tasa_conversion': 0.05
+    },
+    {
+        'nombre': 'Black Friday',
+        'presupuesto': 50000,
+        'inicio': '2023-11-21',
+        'fin': '2023-11-30',
+        'medios': ['Redes Sociales', 'Email Marketing', 'Publicidad en línea'],
+        'segmentos_objetivo': ['Todos los compradores'],
+        'personas_alcanzadas': 100000,
+        'tasa_conversion': 0.03
+    },
+    {
+        'nombre': 'Navidad',
+        'presupuesto': 60000,
+        'inicio': '2023-12-01',
+        'fin': '2023-12-24',
+        'medios': ['Publicidad en tiendas físicas', 'Redes Sociales', 'Publicidad impresa'],
+        'segmentos_objetivo': ['Todos los compradores'],
+        'personas_alcanzadas': 120000,
+        'tasa_conversion': 0.04
+    }
+]
+#EJECICIO 1
+#Lo primero que nos piden es que veamos el nombre de cada campaña, su presupuesto, el número de personas alcanzadas y su tasa de conversión. Para ello, usamos un bucle FOR anidado en otro FOR
+elementos_pedidos = ["nombre","presupuesto","personas_alcanzadas","tasa_conversion"]
+for campaña in lista_campañas:
+  print(f"Aqui tenemos la campaña: \n")
+  for elemento in elementos_pedidos:
+      print(f"{elemento} : {campaña[elemento]}")
